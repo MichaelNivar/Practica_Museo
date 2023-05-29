6ca01e16d2967ece5768014e851ba5aea2f33eb0
picperru <picperru@lab03-21.gregoriofer.local>
2023-05-29 10:06:27 +0200

    no ideia

---
+++ src/main/java/com/jefe/practicamuseo/OtraPrueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jefe.practicamuseo;
+
+/**
+ *
+ * @author picperru
+ */
+public class OtraPrueba {
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+    }
+}
